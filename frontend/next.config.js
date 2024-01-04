30025a0b3b9db88ee0e74074e82ebf11f1ceaf2d
Jeferson Silva <jeferson.i.silva@gmail.com>
2024-01-04 19:40:38 -0300

    Build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   reactStrictMode: true,
-  // basePath: '/aaa',
+  basePath: '/next-react-test',
 };
 
 module.exports = nextConfig
